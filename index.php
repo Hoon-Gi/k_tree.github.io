5d7a86bab2dae0b5b3a1478bf58e51f71a414ac9
Hoon-Gi <dbsrlgns5531@gmail.com>
2020-09-06 19:42:47 +0900

    update

--- index.php
+++ index.php
@@ -1,39 +1,29 @@
+<?php
+    $lines = @file("test.txt") or $result = "파일을 읽을 수 없습니다.";
+    if ($lines != null) {
+        $result = '<table border="1">';
+        $result .= "<tr><th>나무이름</th><th>개화시기</th><th>꽃차례</th><th>꽃 색깔</th><th>잎의 자라는 형태</th>
+					<th>잎의 형태</th><th>잎의 길이</th><th>잎의 폭</th><th>겹잎</th><th>톱니</th>
+					<th>갈래</th><th>열매 결실시기</th><th>열매 종류</th><th>열매 색깔</th></tr>";
+        for($i = 0; $i < count($lines); $i++){
+            $result .= "<tr>";
+            $arr = explode(";", $lines[$i]);
+            for($j = 0;$j < 14;$j++){
+                $result .= "<td>{$arr[$j]}</td>";
+            }
+            $result .= "</tr>";
+        }
+        $result .= "</table>";
+    }
+?>
 <!DOCTYPE html>
-<html>
-    <head>
+<html lang="ko">
+    <head> 
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+        <title>data table</title>
     </head>
     <body>
-        
-        <form action="r_test.php" method="POST">
-            <label>개화시기:</label>
-            <input name="x1" type="number" min="1" max="12"><br>
-            <label>꽃차례:</label>
-            <input name="x2" type="text"><br>
-            <label>꽃 색깔:</label>
-            <input name="x3" type="text"><br>
-            <label>자라는 형태:</label>
-            <input name="x4" type="text"><br>
-            <label>잎의 형태:</label>
-            <input name="x5" type="text"><br>
-            <label>잎의 길이:</label>
-            <input name="x6" type="text"><br>
-            <label>잎의 폭:</label>
-            <input name="x7" type="text"><br>
-            <label>겹잎 여부:</label>
-            <input name="x8" type="text"><br>
-            <label>톱니 여부:</label>
-            <input name="x9" type="text"><br>
-            <label>갈래:</label>
-            <input name="x10" type="text"><br>
-            <label>열매 결실시기:</label>
-            <input name="x11" type="number" min="1" max="12"><br>
-            <label>열매 종류:</label>
-            <input name="x12" type="text"><br>
-            <label>열매 색깔:</label>
-            <input name="x13" type="text"><br>
-           <input type="submit" value="검색">
-        
-        </form>
+        <h1>나무 데이터</h1>
+        <p><?php echo $result; ?></p>
     </body>
 </html>
